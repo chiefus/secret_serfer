8bbbc58ac0a1708c85029ee8d7cdcb6425531fe8
Andrei Shalkou <andrei.shalkou@camelotls.com>
2021-01-21 10:18:41 +0300

    Hidden sensitive data from console output

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 set -o pipefail
 if [ -z "$REPORT" ]
 then
-      trufflehog3 --no-history -r /rules.yml -f yaml $@ ${TARGETS}
+      trufflehog3 --no-history -r /rules.yml --line-number $@ ${TARGETS} | sed -E -e 's/(.*[0-9])([ ]{1})(.*:) .*/Line:\t\1\nMatch:\t\3 *************/'
 else
       trufflehog3 --line-number --no-history -r /rules.yml -f yaml $@ ${TARGETS} | /yaml2junit.py
 fi
