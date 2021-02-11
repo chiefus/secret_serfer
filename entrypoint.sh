9f90bcaa68eb9e17eda78de271fc0fd58b50f8d2
Andrey Shelkov <andrei.shalkou@camelotls.com>
2021-02-11 14:37:49 +0300

    New Functionality, New Rules

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
 set -o pipefail
+cp /rules.yml /modified_rules.yml
+
+for RULE in $(echo "${EXCLUDE_RULES}")
+do
+      sed -i "/$RULE/d" /modified_rules.yml
+done
+
 if [ -z "$REPORT" ]
 then
-      trufflehog3 --no-history -r /rules.yml --line-number $@ ${TARGETS} | sed -E -e 's/(.*[0-9])([ ]{1})(.*:) .*/Line:\t\1\nMatch:\t\3 *************/'
+      if [ "${SUPPRESS_OUTPUT}" == "true" ]
+      then
+            trufflehog3 --line-number --no-history -r /modified_rules.yml -o /dev/null "$@" "${TARGETS}" --skip-paths '.git/'
+      else
+            trufflehog3 --no-history -r /modified_rules.yml --line-number "$@" "${TARGETS}" --skip-paths '.git/' | sed -r 's/\x1b\[0m// ;s/^([0-9]+)([ ]+).*/Line: \1/'
+      fi 
 else
-      trufflehog3 --line-number --no-history -r /rules.yml -f yaml $@ ${TARGETS} | /yaml2junit.py
+      trufflehog3 --line-number --no-history -r /modified_rules.yml -f yaml "$@" "${TARGETS}" --skip-paths '.git/' | /yaml2junit.py
 fi
